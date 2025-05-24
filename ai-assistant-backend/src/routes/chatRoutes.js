dd74d17abac54c9f7faf64a587dca41d6b3e439a
sherelynn <sherelynnb@missionreadyhq.com>
2025-05-25 11:13:03 +1200

    refactor: move all project files to subdirectory

--- ai-assistant-backend/src/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-
-const { chatController } = require("../controllers/chatController")
-
-// --- Routes ---
-router.post("/", chatController)
-
-module.exports = router
